43dfa56931a0eaaddd930571ccb7f99b84e6b195
judenleif <54800741+judenleif@users.noreply.github.com>
2020-08-31 14:43:50 +0200

    läxa

--- helloworld/helloworld/Program.cs
+++ helloworld/helloworld/Program.cs
@@ -6,7 +6,24 @@ namespace helloworld
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("hello niklas!");
+			Console.WriteLine("Vad heter du?:");
+			string Lucas = Console.ReadLine();
+            Console.WriteLine("Username is: " + Lucas);
+
+			Console.WriteLine("Hur gammal är du:");
+			int age = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Your age is: " + age);
+		
+			bool alive = true;
+            if (alive == true)
+            {
+				Console.WriteLine("lever");
+			}
+            else
+            {
+				Console.WriteLine("död");
+			}
+		
 		}
 	}
 }
